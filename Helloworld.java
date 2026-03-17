39e34f67e51de7d3e50347f9f9d756f07245bbe4
Shashwat14100 <st5248@srmist.edu.in>
2026-03-17 13:58:11 +0530

    UC2 COMPLETED

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,10 @@
-public class Helloworld{
-    public static void main(String[] args){
-        System.out.println("Hello World");
+public class Helloworld {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        }
     }
 }
